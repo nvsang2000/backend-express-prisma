d1b4ed5c97a1e13e0684afd6444466e14d501ddf
nvsang260720 <nvsang2670@gmail.com>
2023-07-02 22:34:58 +0700

    fix config

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import dotenv from "dotenv";
-import express from "express";
+import express, { Request, Response } from "express";
 import userRouter from "./routes/user";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+app.get("/", (_req: Request, res: Response) => {
+  return res.send("Express Typescript on Vercel");
+});
 app.use("/api", userRouter);
-
 app.listen(3000 || process.env.PORT, () => {
   console.log(
     `Server successfully listening at http://localhost:${process.env.PORT}`
